a4458325df157a62fc6991d2f3cb737df5a4d104
wwiiiii <wwiiiii@naver.com>
2014-07-17 20:21:22 +0900

    아까한거 다시
    ㅇㅇ

---
+++ Road.cpp
@@ -0,0 +1,8 @@
+#include "Road.h"
+#include <vector>
+#include <utility>
+
+std::pair<double, double > cRoad::RoadForce(cRobot& target){
+	pair<double, double> targetCoord = target.fNowCoord();
+
+}
--- Road.h
+++ Road.h
@@ -1,18 +1,12 @@
 #ifndef __Road_H__
 #define __Road_H__
 
-#ifndef <vector>
-#define <vector>
-#endif
-
 typedef struct{ double a, b, c; }line;
 
 class cRoad{
-	public:
-		std::vector<line> Lines;
-	private:
-}
-
-
+public:
+	std::vector<line> Lines;
+	std::pair<double, double> RoadForce(cRobot& target);
+};
 
 #endif
